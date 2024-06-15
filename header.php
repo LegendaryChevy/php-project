31b1c8fab6108fd90ac3bc2579e29a6692140886
legendarychevy <leeson73@gmail.com>
2024-06-14 19:13:36 -0700

    got the header looking better

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <div id=header>
     
     <h1>Dynamic logins</h1>
-    <nav>
+    <nav id="nav_bar">
         <a href="gallery.html">Gallery</a>
         <a href="contact_us.html">Contact Us</a>
         <a href="book_an_appointment.html">Book An Appointment</a>
